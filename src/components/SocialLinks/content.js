e4dc1eb5f49c73152c295fc25453e27f7a86903b
Gabriel <mfkrgabriel@gmail.com>
2019-09-03 16:59:27 -0300

    Social links updated

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,24 @@
+const links = [
+  {
+    label: 'Github',
+    url: 'https://github.com/gabriel-brito',
+  },
+  {
+    label: 'Twitter',
+    url: 'https://github.com/gabriel-brito',
+  },
+  {
+    label: 'Youtube',
+    url: 'https://github.com/gabriel-brito',
+  },
+  {
+    label: 'Instagram',
+    url: 'https://github.com/gabriel-brito',
+  },
+  {
+    label: 'Unsplash',
+    url: 'https://github.com/gabriel-brito',
+  },
+];
+
+export default links;
