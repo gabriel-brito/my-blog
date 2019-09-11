ee2a3f6fa313fbcd8fad76f19603a6874ed0d784
Gabriel <mfkrgabriel@gmail.com>
2019-09-11 13:38:15 -0300

    Add algolia

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+const queries = require('./src/utils/algolia_queries');
+
 module.exports = {
   siteMetadata: {
     title: `My Blog`,
@@ -55,6 +58,17 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-algolia-search`,
+      options: {
+        appId: process.env.ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_ADMIN_KEY,
+        indexName: process.env.ALGOLIA_INDEX_NAME,
+        queries,
+        chunkSize: 10000, // default: 1000
+        enablePartialUpdates: true,
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
---
+++ src/utils/algolia_queries.js
@@ -0,0 +1,44 @@
+const postsQuery =`{
+  posts: allMarkdownRemark(
+    sort: {fields: frontmatter___date, order: DESC}
+    ) {
+    edges {
+      node {
+        objectID: id
+        fields {
+          slug
+        }
+        frontmatter {
+          category
+          date_timestamp: date
+          date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+          title
+          description
+        }
+        excerpt(pruneLength: 5000)
+      }
+    }
+  }
+}`;
+
+const flatten = arr =>
+  arr.map(({ node: { frontmatter, ...rest }}) => ({
+    ...frontmatter,
+    data_timestamp: parseInt(
+      (new Date(frontmatter.data_timestamp).getTime() / 1000).toFixed(0)
+    ),
+    ...rest
+  }))
+
+const queries = [
+  {
+    query: postsQuery,
+    transformer: ({ data }) => flatten(data.posts.edges),
+    indexName: 'posts', // overrides main index name, optional
+    settings: {
+      attributesToSnippet: ['excerpt:20']
+    },
+  },
+];
+
+module.exports =  queries;
